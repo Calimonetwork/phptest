c2de4a567ba649df6c3e8ef82d5abad5c229bea5
Calimonetwork <108202311+Calimonetwork@users.noreply.github.com>
2022-07-10 14:57:09 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	include( $_SERVER['DOCUMENT_ROOT'].'/package/inc/header.php' );
+?>
+		<div style="text-align:center;">
+			<h2><?php echo $_WIKI['NAME']?>에 오신걸 환영합니다!</h2>
+			<p><?php echo $_WIKI['NAME']?>는 누구나 수정할 수 있게 하는 자유지향적인 위키입니다.<br>
+			단, 반달 행위는 엄격히 금지됩니다.<br>
+			+ <?php echo $_WIKI['NAME']?>는 백과사전이 아닙니다. 일부 잘못된 서술이 있을 수 있습니다.
+		</div>
+<?php
+	include( $_SERVER['DOCUMENT_ROOT'].'/package/inc/footer.php' );
+?>
